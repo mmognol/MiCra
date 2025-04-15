d257c6799b65a959e86e5e4107fcb1f389867633
Meven MOGNOL <mmognol@upmem.com>
2025-04-15 16:04:49 +0200

    refactor WramAligned to use MRAM_ALIGNMENT_BYTES and improve syntax

--- src/dpu/wram_aligned.hpp
+++ src/dpu/wram_aligned.hpp
@@ -1,15 +1,17 @@
 #ifndef DE684C30_2F56_47BF_B9EC_71F06675B8A1
 #define DE684C30_2F56_47BF_B9EC_71F06675B8A1
 
-template <typename T, unsigned int S>
-struct alignas(8) WramAligned
-{
-    T buffer[S];
-    T &operator[](int i) { return buffer[i]; }
-    const T &operator[](int i) const { return buffer[i]; }
+#include "syslib/mram.hpp"
 
-    T *begin() { return &buffer[0]; }
-    T *end() { return &buffer[S]; }
+template <typename T, unsigned int S>
+struct alignas(MRAM_ALIGNMENT_BYTES) WramAligned
+{
+    T buffer[S]; //NOLINT(modernize-avoid-c-arrays)
+    auto operator[](int i) -> T & { return buffer[i]; }
+    auto operator[](int i) const -> const T & { return buffer[i]; }
+
+    auto begin() -> T * { return &buffer[0]; }
+    auto end() -> T * { return &buffer[S]; }
 };
 
 #endif /* DE684C30_2F56_47BF_B9EC_71F06675B8A1 */
